e07334de0e14dea40e8bd7c750d60351a8e07fa7
ardamoin <ardamoin2@gmail.com>
2023-06-20 17:43:22 -0400

    Add tailwind prettier plugin and add styling to header and footer components

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,15 +1,18 @@
-import './Footer.scss';
-
 const Footer = () => {
   return (
     <footer className="footer">
-        <p>
-            Copyright © {new Date().getFullYear()} ardamoin <a href="https://github.com/ardamoin" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-github"></i>
-            </a>
-        </p>
+      <p className="relative bottom-0 left-0 mt-8 flex w-full items-center justify-center gap-[10px] p-2 font-montserrat text-xl">
+        Copyright © {new Date().getFullYear()} ardamoin{" "}
+        <a
+          href="https://github.com/ardamoin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-github text-2xl text-[#191919] transition-transform duration-[0.3s] ease-[ease-in-out] hover:rotate-[360deg] hover:scale-[1.2]"></i>
+        </a>
+      </p>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -2,12 +2,22 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="flex shrink-0 gap-5 items-center px-3 py-5 bg-secondary min-w-[650px]">
-      <h1 className="text-4xl text-primary font-bold font-barlow mr-auto ml-5">Hasbulla Store</h1>
-      <Link to="/" className="font-montserrat hover:text-primary">Homepage</Link>
-      <Link to="products" className="font-montserrat hover:text-primary">Products</Link>
-      <Link to="cart" className="font-montserrat hover:text-primary">Cart</Link>
-      <Link to="contact" className="font-montserrat mr-5 hover:text-primary">Contact Us</Link>
+    <div className="flex min-w-[650px] shrink-0 items-center gap-5 bg-secondary px-3 py-5">
+      <h1 className="ml-5 mr-auto font-barlow text-4xl font-bold text-primary">
+        Hasbulla Store
+      </h1>
+      <Link to="/" className="font-montserrat hover:text-primary">
+        Homepage
+      </Link>
+      <Link to="products" className="font-montserrat hover:text-primary">
+        Products
+      </Link>
+      <Link to="cart" className="font-montserrat hover:text-primary">
+        Cart
+      </Link>
+      <Link to="contact" className="mr-5 font-montserrat hover:text-primary">
+        Contact Us
+      </Link>
     </div>
   );
 };
