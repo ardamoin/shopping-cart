4cf10c2e8694f6c1b8f8e334499c3624e9e1297b
ardamoin <ardamoin2@gmail.com>
2023-06-21 18:24:40 -0400

    Add styling

--- src/Components/Shop/Product.jsx
+++ src/Components/Shop/Product.jsx
@@ -4,7 +4,7 @@ const Product = ({ productImg, productName, productPrice }) => {
     <div className="flex max-w-fit min-w-[275px] flex-col gap-2 rounded border-[1px] border-solid border-gray-500 bg-secondary p-4 font-montserrat">
       <img src={productImg} alt="product img" className="h-60" />
       <h2 className="text-lg font-semibold">{productName}</h2>
-      <p>{`$${productPrice}`}</p>
+      <p className="font-normal">{`$${productPrice}`}</p>
       <button
         type="button"
         className="rounded bg-primary py-2 font-semibold text-white hover:opacity-75"
