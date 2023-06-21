d7967b139e25458a72eb3d20479cf4ba7dfbdf59
ardamoin <ardamoin2@gmail.com>
2023-06-20 20:27:36 -0400

    Update footer styling

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,14 +1,14 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <p className="relative bottom-0 left-0 mt-8 flex w-full items-center justify-center gap-[10px] p-2 font-montserrat text-xl">
+      <p className="relative bottom-0 left-0 flex w-full items-center justify-center gap-[10px] p-3 font-montserrat text-l bg-slate-900 text-white">
         Copyright © {new Date().getFullYear()} ardamoin{" "}
         <a
           href="https://github.com/ardamoin"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i className="fab fa-github text-2xl text-[#191919] transition-transform duration-[0.3s] ease-[ease-in-out] hover:rotate-[360deg] hover:scale-[1.2]"></i>
+          <i className="fab fa-github text-2xl text-white transition-transform duration-[0.3s] ease-[ease-in-out] hover:rotate-[360deg] hover:scale-[1.2]"></i>
         </a>
       </p>
     </footer>
