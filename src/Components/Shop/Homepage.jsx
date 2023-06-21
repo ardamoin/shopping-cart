f4e420d34f51bb026f0191c18ba5907a17adf355
ardamoin <ardamoin2@gmail.com>
2023-06-20 20:10:56 -0400

    Add styling and content to Homepage and Page components

--- src/Components/Shop/Homepage.jsx
+++ src/Components/Shop/Homepage.jsx
@@ -1,9 +1,25 @@
 import Page from "../UI/Page";
+import { Link } from "react-router-dom";
+import hasbikMain from "../../Assets/hasbulla_main.jpg";
 
 const Homepage = () => {
   return (
-    <Page>
-      <div>Homepage</div>
+    <Page className="gap-[100px] min-w-[600px]">
+      <div className="flex flex-col items-start gap-5">
+        <h1 className="font-barlow text-4xl font-semibold text-primary">
+          For all things Hasbulla
+        </h1>
+        <p className="font-montserrat">
+          Check out our vast selection of Hasbulla merch
+        </p>
+        <Link
+          to="/products"
+          className="mb-2 mr-2 rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 px-5 py-2.5 text-center font-montserrat text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:focus:ring-cyan-800"
+        >
+          Shop Now
+        </Link>
+      </div>
+      <img src={hasbikMain} className="h-3/4 rounded-full"></img>
     </Page>
   );
 };
--- src/Components/UI/Page.jsx
+++ src/Components/UI/Page.jsx
@@ -1,6 +1,12 @@
 /* eslint-disable react/prop-types */
 const Page = (props) => {
-  return <div className="h-screen">{props.children}</div>;
+  return (
+    <div
+      className={`flex h-screen items-center justify-center bg-gradient-to-b from-transparent via-stone-300 to-neutral-400 ${props.className}`}
+    >
+      {props.children}
+    </div>
+  );
 };
 
 export default Page;
