3ec5df76f1e6a5d1735a2985c84a9d78f65b4024
ardamoin <ardamoin2@gmail.com>
2023-06-25 16:00:47 -0400

    Add functionality to the cart link on the header component so that whenever user adds a product to the cart, the total number of items in the cart is displayed next to the cart link.

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,9 +1,26 @@
 import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import CartContext from "../../context/cart-context";
 
 const Header = () => {
+  const { shoppingCart } = useContext(CartContext);
+  const [totalCount, setTotalCount] = useState(0);
+
+  useEffect(() => {
+    setTotalCount(
+      Object.keys(shoppingCart).reduce(
+        (total, current) => total + shoppingCart[current].productCount,
+        0
+      )
+    );
+  }, [shoppingCart]);
+
   return (
     <div className="flex min-w-[650px] shrink-0 items-center gap-5 bg-secondary px-3 py-5">
-      <Link to="/" className="ml-5 mr-auto font-barlow text-4xl font-bold text-primary">
+      <Link
+        to="/"
+        className="ml-5 mr-auto font-barlow text-4xl font-bold text-primary"
+      >
         Hasbulla Store
       </Link>
       <Link to="/" className="font-montserrat hover:text-primary">
@@ -14,6 +31,7 @@ const Header = () => {
       </Link>
       <Link to="cart" className="font-montserrat hover:text-primary">
         Cart
+        {totalCount !== 0 && `(${totalCount})`}
       </Link>
       <Link to="contact" className="mr-5 font-montserrat hover:text-primary">
         Contact Us
