328bd3b11add4d6aa1294684fd443dd0f59d4daf
ardamoin <ardamoin2@gmail.com>
2023-06-25 14:10:22 -0400

    Make styling changes

--- src/Components/Shop/Products.jsx
+++ src/Components/Shop/Products.jsx
@@ -9,36 +9,38 @@ const Products = () => {
   return (
     <Page className="items-start justify-center overflow-hidden">
       <table cellPadding="20px">
-        <tr> 
-          <th>
-            <Product
-              productImg={mug}
-              productName="Hasbulla Mug"
-              productPrice={12.45}
-            />
-          </th>
-          <th>
-            <Product
-              productImg={pillow}
-              productName="Hasbulla Pillow"
-              productPrice={23.99}
-            />
-          </th>
-          <th>
-            <Product
-              productImg={tapestry}
-              productName="Hasbulla Tapestry"
-              productPrice={19.45}
-            />
-          </th>
-          <th>
-            <Product
-              productImg={cutout}
-              productName="Hasbulla Cutout"
-              productPrice={7.55}
-            />
-          </th>
-        </tr>
+        <tbody>
+          <tr>
+            <th>
+              <Product
+                productImg={mug}
+                productName="Hasbulla Mug"
+                productPrice={12.45}
+              />
+            </th>
+            <th>
+              <Product
+                productImg={pillow}
+                productName="Hasbulla Pillow"
+                productPrice={23.99}
+              />
+            </th>
+            <th>
+              <Product
+                productImg={tapestry}
+                productName="Hasbulla Tapestry"
+                productPrice={19.45}
+              />
+            </th>
+            <th>
+              <Product
+                productImg={cutout}
+                productName="Hasbulla Cutout"
+                productPrice={7.55}
+              />
+            </th>
+          </tr>
+        </tbody>
       </table>
     </Page>
   );
