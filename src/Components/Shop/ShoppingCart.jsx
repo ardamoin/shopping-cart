1200ac0eb98bbbf6f65063776f4316f1263d596b
ardamoin <ardamoin2@gmail.com>
2023-06-25 14:13:52 -0400

    Add cart item components through shopping cart context

--- src/Components/Shop/ShoppingCart.jsx
+++ src/Components/Shop/ShoppingCart.jsx
@@ -1,8 +1,28 @@
+import CartContext from "../../context/cart-context";
 import Page from "../UI/Page";
+import { useContext } from "react";
+import CartItem from "./CartItem";
 
 const ShoppingCart = () => {
+  const { shoppingCart } = useContext(CartContext);
+
   return (
     <Page className="flex flex-col pl-10">
+      <ul>
+        {Object.keys(shoppingCart).map((product) => {
+          if (shoppingCart[product].productCount > 0) {
+            return (
+              <li key={Math.random()}>
+                <CartItem
+                  productName={product}
+                  productCount={shoppingCart[product].productCount}
+                  productPrice={shoppingCart[product].productPrice}
+                />
+              </li>
+            );
+          }
+        })}
+      </ul>
     </Page>
   );
 };
