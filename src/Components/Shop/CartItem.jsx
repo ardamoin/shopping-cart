98b10970764bb9b6b98a5ee187d141e72f0c2683
ardamoin <ardamoin2@gmail.com>
2023-06-25 14:15:08 -0400

    Add product image sources

--- src/Components/Shop/CartItem.jsx
+++ src/Components/Shop/CartItem.jsx
@@ -1,12 +1,22 @@
 /* eslint-disable react/prop-types */
 import { useRef, useState } from "react";
-import tapestry from "../../Assets/tapestry.jpg";
+import HasbullaTapestry from "../../Assets/tapestry.jpg";
+import HasbullaMug from "../../Assets/mug.jpg";
+import HasbullaPillow from "../../Assets/pillow.jpg";
+import HasbullaCutout from "../../Assets/cutout.jpg";
 import ItemCount from "../UI/ItemCount";
 
 const CartItem = ({ productName, productCount, productPrice }) => {
   const [count, setCount] = useState(productCount);
   const [price, setPrice] = useState(productPrice * productCount);
   const itemRef = useRef();
+
+  const itemImages = {
+    "Hasbulla Tapestry": HasbullaTapestry,
+    "Hasbulla Pillow": HasbullaPillow,
+    "Hasbulla Mug": HasbullaMug,
+    "Hasbulla Cutout": HasbullaCutout
+  }
 
   const addItemHandler = () => {
     setCount((prev) => prev + 1);
@@ -23,10 +33,10 @@ const CartItem = ({ productName, productCount, productPrice }) => {
 
   return (
     <div ref={itemRef} className="flex gap-10 items-center my-5">
-      <img src={tapestry} alt="img" className="h-[120px]" />
+      <img src={itemImages[productName]} alt="img" className="h-[120px] w-[120px]" />
       <span className="flex flex-col gap-2">
         <h2 className="font-montserrat font-semibold text-lg">{productName}</h2>
-        <p>{`$${price}`}</p>
+        <p>{`$${price.toFixed(2)}`}</p>
         <ItemCount
           value={count}
           onAdd={addItemHandler}
